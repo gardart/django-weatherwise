ad956430af14f45200e6a08e8e62e0f9d6ec7d33
Garðar Þorsteinsson <gardar.thorsteinsson@arionbanki.is>
2025-12-03 21:52:25 +0000

    add front page

--- weatherwise/urls.py
+++ weatherwise/urls.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from django.urls import path
 
+from weatherwise.weatherwane.views import observations_list
+
 urlpatterns = [
+    path("", observations_list, name="observations_list"),
     path("admin/", admin.site.urls),
 ]
